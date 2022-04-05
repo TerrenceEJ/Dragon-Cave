ed55bda82e1a93385a26cfc31b47fddef26d26bc
TerrenceEJ <terrencejones0820@gmail.com>
2022-04-05 09:49:57 -0400

    initial

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("You are in a land full of dragons. In front of you, \nyou see two caves. In one cave, the dragon is friendly \nand will share his treasure with you." +
+                "The other dragon \nis greedy and hungry and will eat you on sight. \nWhich cave will you go into? (1 or 2)");
+        Scanner getInput = new Scanner(System.in);
+        Integer input = getInput.nextInt();
+        System.out.println("You approach the cave... \nIt is dark and spooky... \nA large dragon jumps out in front of you! He opens his jaws and... \nGobbles you down in one bite!");
+
+    }
+}
