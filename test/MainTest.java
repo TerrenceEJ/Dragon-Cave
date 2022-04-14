ef1a794f5090e2dc40d58cd2e7762644465e29c3
TerrenceEJ <terrencejones0820@gmail.com>
2022-04-13 20:51:07 -0400

    checking if goes past while loop

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,35 +1,24 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.*;
+import static org.hamcrest.CoreMatchers.*;
 
-import java.util.Scanner;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class MainTest {
     Main main = new Main();
 
-    @Test void scan(){
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        System.out.println(num);
-    }
-
-    @Test void setMain(){
-    }
-
     @Test
     void main() {
-        boolean x = false;
-        Assertions.assertTrue(Main.run(x), "true");
+        boolean x;
+        x = run();
         System.out.println(x);
     }
 
     @Test
-    boolean run(boolean x) {
-        //System.out.println(x);
-        //assertEquals("You approach the cave... \nIt is dark and spooky... \nA large dragon jumps out in front of you! He opens his jaws and... \nGobbles you down in one bite!", run(x), "Successful output.");
-        Assertions.assertTrue(true, "Unsuccessful output");
-        //assertEquals("You approach the cave... \nIt is dark and spooky... \nA large dragon jumps out in front of you! He opens his jaws and... \nGobbles you down in one bite!", run(x), x == true);
-        System.out.println(x);
+    boolean run() {
+        boolean x = true;
+        main.x = x;
+        main.run(x);
+        assertThat(x,is(true));
         return x;
     }
 }
